0fcfaee09219773b92102261563e5cb33226051c
mdhole <manthan.dhole@infor.com>
2024-01-14 01:49:11 +0530

    Types of component selector: HTML Tag, HTML Attribute, CSS Class, ID

--- 01_NewAngularProject/angular-ekart/src/app/top-header/top-header.component.ts
+++ 01_NewAngularProject/angular-ekart/src/app/top-header/top-header.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'top-header',
+  // selector: 'top-header', //HTML Tag
+  // selector: '[top-header]', //HTML Attribute
+  // selector: '.top-header', //CSS class
+  selector: '#top-header', //ID
   standalone: true,
   imports: [],
   templateUrl: './top-header.component.html',
