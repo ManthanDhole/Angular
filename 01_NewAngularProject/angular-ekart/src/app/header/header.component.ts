dbcb106fe4e1ed3d537806e36d5a2d060079d6d0
mdhole <manthan.dhole@infor.com>
2024-01-13 17:19:00 +0530

    Adding styles to the html page using styles, styleUrls

--- 01_NewAngularProject/angular-ekart/src/app/header/header.component.ts
+++ 01_NewAngularProject/angular-ekart/src/app/header/header.component.ts
@@ -4,6 +4,13 @@ import { Component } from "@angular/core";
     selector: 'app-header',
     // template: '<h3>Ekart Application Header</h3>',
     templateUrl: './header.component.html',
+    // styles: [ 
+        // 'h3{ margin: 0 20px; color: red; }', 
+        // 'button{ margin: 5px 20px; padding: 10px; color: blue; }',
+        // '.headerClass{ color: green; }',
+        // '#idName{ color: blue; }'
+    // ],
+    styleUrls: [ './header.component.css' ],
     standalone: true
 })
 
