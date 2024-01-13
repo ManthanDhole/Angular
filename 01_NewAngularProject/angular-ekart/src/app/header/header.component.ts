aa7bb9b664092bc81518b6c1371ffe0f5c0a69d2
mdhole <manthan.dhole@infor.com>
2024-01-13 16:45:24 +0530

    What is a Component and View Template

---
+++ 01_NewAngularProject/angular-ekart/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    // template: '<h3>Ekart Application Header</h3>',
+    templateUrl: './header.component.html',
+    standalone: true
+})
+
+export class HeaderComponent {
+
+}
