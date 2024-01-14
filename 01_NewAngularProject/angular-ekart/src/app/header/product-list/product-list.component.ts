9f6290d84a4560ca3e5c96f41e3a7f23d350ba9a
mdhole <manthan.dhole@infor.com>
2024-01-14 16:06:52 +0530

    Data Binding: One Way Data Binding: Event Binding

--- 01_NewAngularProject/angular-ekart/src/app/header/product-list/product-list.component.ts
+++ 01_NewAngularProject/angular-ekart/src/app/header/product-list/product-list.component.ts
@@ -17,14 +17,50 @@ export class ProductListComponent {
       color:'Blue',
       price: 995,
       discount: 10,
-      inStock: 0,
+      inStock: 5,
       image: 'https://media.croma.com/image/upload/v1662424212/Croma%20Assets/Communication/Mobiles/Images/234252_czgdvc.png'
     }
 
     customerName = "Manthan";
+
+    addToCart: number = 0;
+    disableDecrementButton: boolean = true;
+    disableIncrementButton: boolean = false;
+
     getDiscount() {
       //In order to access an object we need to specify this. keyword to point our funtion to use object inside this class
       return this.product.price - (this.product.price * this.product.discount) /100
     }
     
+    onNameChanged(event: any){
+      // console.log(event);
+      // console.log(event.target.value);
+      // this.customerName = event.target.value;
+    }
+
+    decreaseQuantity(){
+      if(this.addToCart > 0)
+      {
+        this.addToCart -= 1;
+        this.disableDecrementButton = false;
+        this.disableIncrementButton = false;
+      }
+      if(this.addToCart == 0)
+      {
+        this.disableDecrementButton = true;
+      }
+    }
+    
+    increaseQuantity(){
+      if(this.addToCart < this.product.inStock)
+      {
+        this.addToCart += 1;
+        this.disableIncrementButton = false;
+        this.disableDecrementButton = false;
+      }
+      if(this.addToCart == this.product.inStock)
+      {
+        this.disableIncrementButton = true;
+      }
+    }
 }
